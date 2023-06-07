54d12e34707b6c8880ca051decfb21c4afec6fae
FHPopova21 <FHPopova21@codingburgas.bg>
2023-06-08 00:15:23 +0300

    Add JS file

---
+++ Tech Monkeys/js.js
@@ -0,0 +1,22 @@
+document.getElementById("btn").addEventListener("click", function() {
+    var form = document.getElementById("form");
+    form.classList.toggle("hidden");
+});
+
+document.getElementById("btn").addEventListener("click", function() {
+    var form = document.getElementById("form");
+    form.classList.toggle("form");
+
+    var btn = document.getElementById("btn");
+    btn.style.display = "none";
+});
+
+document.addEventListener("keydown", function(event) {
+    if (event.key === "Escape") {
+      var form = document.getElementById("form");
+      form.classList.add("form");
+
+      var btn = document.getElementById("btn");
+      btn.style.display = "block";
+    }
+});
